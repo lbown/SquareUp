0e867b18f08b2357a9dd182eb51c487b72ebf9c7
Redzango <cjohns10@trinity.edu>
2020-03-26 15:29:18 -0500

    player movement

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController cc;
+    public float speed = 12f;
+    public float gravity = -9.8f*3;
+    public float jumpHeight = 2;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGround;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * x;
+
+        cc.Move(move * speed * Time.deltaTime);
+    }
+
+    private void OnJump()
+    {
+        Debug.Log("Jump");
+        if (isGround)
+        {
+            Debug.Log("Jump");
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
