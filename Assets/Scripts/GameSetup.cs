19a31f02dd77c251e861048c32f30adee8e53fc3
Chase Wilson <cwilson7@trinity.edu>
2020-03-27 14:41:15 -0500

    edited coopers scene

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup gs;
+
+    public Transform[] spawnPoints;
+
+    private void OnEnable()
+    {
+        if(GameSetup.gs == null)
+        {
+            GameSetup.gs = this;
+        } 
+    }
+}
---
+++ Assets/Scripts/PhotonPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PhotonPlayer : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myAvatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        int spawnPicker = Random.Range(0, GameSetup.gs.spawnPoints.Length);
+        if(PV.IsMine)
+        {
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), GameSetup.gs.spawnPoints[spawnPicker].position, GameSetup.gs.spawnPoints[spawnPicker].rotation, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
