8613487d08c40db9b0c561ff86c9e3fad837d03e
Chase Wilson <cwilson7@trinity.edu>
2020-04-14 18:57:18 -0500

    actually fixed colin approved 10/10

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -33,7 +33,6 @@ public class CubeController : MonoBehaviourPun
        if(PV.IsMine) { 
         if (inRotation)
             {
-                PhotonNetwork.Destroy(GameObject.Find("RotateCubePowerUp(Clone)"));
                 gameObject.transform.rotation = cubeRot;
 
                 targetXY = new Vector2(rubberBandX(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")),
@@ -67,6 +66,7 @@ public class CubeController : MonoBehaviourPun
         gm.pauseTime();
         inRotation = true;
         gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0, 20);
+        PhotonNetwork.Destroy(GameObject.Find("RotateCubePowerUp(Clone)"));
     }
 
     void StopRotation()
