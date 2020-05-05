98a16b7f0c2cae9b402547c6a6a2f74f503930d2
Chase <cwilson7@trinity.edu>
2020-05-05 15:52:06 -0700

    Set up character selection screen

---
+++ Assets/Scripts/CharSelectButtonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharSelectButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public TMP_Text btnText, charInfo;
+    private Color original;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        original = btnText.color;
+        charInfo.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        btnText.color = new Color(0, 0, 0);
+        charInfo.enabled = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        btnText.color = original;
+        charInfo.enabled = false;
+    }
+
+    
+}
--- Assets/Scripts/CharSelectionController.cs
+++ Assets/Scripts/CharSelectionController.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Photon.Pun;
+using TMPro;
+using UnityEngine.UI;
 
 public class CharSelectionController : MonoBehaviour
 {
     public Canvas canvas;
     public GameObject panel;
+    public Button[] buttons;
+    public List<string> charNames;
+    public TMP_Text timer, waitingTxt, readyTxt;
     public bool readyToSpawn;
     private GameManager gm;
 
     private void Awake()
     {
         readyToSpawn = false;
+        timer.enabled = false;
+        waitingTxt.enabled = false;
+        readyTxt.color = Color.green;
+        readyTxt.enabled = false;
+        buttons = panel.GetComponentsInChildren<Button>();
+        charNames = new List<string> { "Dash", "Sticky", "Double", "Warp" };
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
@@ -21,10 +32,18 @@ public class CharSelectionController : MonoBehaviour
     {
         if (PlayerInfo.PI != null)
         {
+            waitingTxt.enabled = true;
+            readyTxt.enabled = true;
             readyToSpawn = true;
+            foreach(Button btn in buttons)
+            {
+                btn.GetComponent<CharSelectButtonController>().charInfo.text = charNames[whichCharacter] + " Selected";
+                btn.gameObject.SetActive(false);
+            }
             PlayerInfo.PI.mySelectedCharacter = whichCharacter;
             PlayerPrefs.SetInt("SelectedCharacter", whichCharacter);
-            panel.SetActive(false);
         }
     }
+
+
 }
