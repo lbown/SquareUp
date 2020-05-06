2ff8f8005ef4a2de1bcb94af704bb73d8f27b541
Chase <cwilson7@trinity.edu>
2020-05-06 15:36:25 -0700

    added players in lobby text

---
+++ Assets/NumberOfPlayersInRoomTxt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class NumberOfPlayersInRoomTxt : MonoBehaviour
+{
+    private TMP_Text thisText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisText = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisText.text = "Number of players in lobby: " + PhotonNetwork.PlayerList.Length;
+    }
+}
