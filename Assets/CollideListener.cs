dc1b674199f8dd5e6d7b676d744112fb62614c1b
Logan Bown <lbown@trinity.edu>
2020-05-06 15:09:10 -0500

    Added more splash

--- Assets/CollideListener.cs
+++ Assets/CollideListener.cs
@@ -7,7 +7,7 @@ public class CollideListener : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] private GameObject bloodObj;
+    [SerializeField] private GameObject[] bloodObjs;
     public LayerMask canvasMask;
     public LayerMask groundMask;
     public GameObject cubeTransform;
@@ -34,8 +34,11 @@ public class CollideListener : MonoBehaviour
                 mat.SetColor("_BaseColor", other.GetComponent<ParticleSystem>().startColor);
                 //mat.SetColor("_EmissionColor", other.GetComponent<ParticleSystem>().startColor);
                 //mat.EnableKeyword("_EMISSION");
+                GameObject bloodObj = bloodObjs[Random.Range(0, bloodObjs.Length)];
                 GameObject blood = Instantiate(bloodObj, cubeTransform.transform);
-                blood.GetComponent<MeshRenderer>().sharedMaterial = mat;
+                foreach (MeshRenderer b in blood.GetComponentsInChildren<MeshRenderer>()) {
+                    b.sharedMaterial = mat;
+                }
                 //int otherPlayerID = other.GetComponent<ParticleSystem>().gameObject.GetComponentInParent<CharacterMovement>().lastShotMe;
                 //Debug.Log("This is other player id= " + otherPlayerID);
                 //int otherPlayerColorID = PhotonView.Find(otherPlayerID).gameObject.GetComponent<PhotonPlayer>().myAvatar.GetComponent<CharacterMovement>().colorID;
@@ -46,7 +49,10 @@ public class CollideListener : MonoBehaviour
 
                 //Debug.Log(other.GetComponent<ParticleSystem>().startColor == blood.GetComponent<MeshRenderer>().sharedMaterial.color);
                 blood.transform.position = p.intersection;
+                blood.transform.Rotate(Vector3.up, Random.Range(0, 360));
                 blood.transform.rotation = Quaternion.FromToRotation(Vector3.up, p.normal);
+                float scaleFactor = Random.RandomRange(0.75f, 1.25f);
+                blood.transform.localScale.Scale(new Vector3(scaleFactor, 1, scaleFactor));
                 bool offEdge = false;
                 foreach (Transform t in blood.transform)
                 {
